d0167e0e42c732655668ead5d9863f84cbb7c26a
Sumit Gohil <gohilsumit142@gmail.com>
2024-08-19 09:09:08 +0530

    Update ticket_insert.php

--- railway/ticket_insert.php
+++ railway/ticket_insert.php
@@ -4,7 +4,7 @@ $user=$_POST['user'];
 $toDes=$_POST['to_d'];
 $fromDes=$_POST['from_d'];
 $price=$_POST['price'];
-$insert="INSERT INTO `ticket`(`name`, `to_d`, `from_d`,`price`) VALUES ('$name','$toDes','$fromDes','$price')";
+$insert="INSERT INTO `ticket`(`user`, `to_d`, `from_d`,`price`) VALUES ('$user','$toDes','$fromDes','$price')";
 if($user !=null && $toDes !=null && $fromDes !=null && $price !=null){
 $a=mysqli_query($conn,$insert); 
 if($a)
